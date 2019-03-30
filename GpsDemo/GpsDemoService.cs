38eac90815684a80eb1776fb5a0dfc481a1502be
Doug Wilson <d@dkw.io>
2019-03-30 13:01:40 -0700

    Added GpsDemo project

---
+++ GpsDemo/GpsDemoService.cs
@@ -0,0 +1,51 @@
+﻿using DKW.NMEA;
+using DKW.NMEA.GasFinder;
+using DKW.NMEA.GPS;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RJCP.IO.Ports;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GpsDemo
+{
+    public sealed class GpsDemoService : BackgroundService
+    {
+        private readonly ILogger<GpsDemoService> _logger;
+        private readonly NmeaStreamReader _nsr;
+
+        public GpsDemoService(ILogger<GpsDemoService> logger, NmeaStreamReader nmeaStreamReader)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _nsr = nmeaStreamReader ?? throw new ArgumentNullException(nameof(nmeaStreamReader));
+            _nsr.Register(new GFDTA(), new GGA(), new GLL(), new GSA(), new GSV(), new RMC(), new VTG());
+            _nsr.AbortAfterUnparsedLines = 10;
+            _logger.LogInformation("Available Ports: {Ports}", String.Join(", ", SerialPortStream.GetPortNames()));
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken = default)
+        {
+            var portName = SerialPortStream.GetPortNames().First();
+            _logger.LogInformation("Opening {0}", portName);
+            using (var port = new SerialPortStream(portName))
+            {
+                port.Open();
+                port.Handshake = Handshake.None;
+                port.NewLine = "\r\n";
+                port.ReadTimeout = 5000;
+                port.Write("\r\n");
+
+                var exitReason = await _nsr.ParseStreamAsync(port, async (message) => await DispatchMessage(message).ConfigureAwait(false), stoppingToken).ConfigureAwait(false);
+                _logger.LogInformation("Exit Reason: {ExitReason}", exitReason);
+            }
+        }
+
+        private Task DispatchMessage(NmeaMessage message)
+        {
+            _logger.LogInformation(message.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ GpsDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using DKW.NMEA;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GpsDemo
+{
+    internal static class Program
+    {
+        public static async Task Main(String[] args)
+        {
+            var useConsole = Debugger.IsAttached || args.Contains("--console");
+
+            var builder = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddTransient<NmeaStreamReader>();
+                    services.AddHostedService<GpsDemoService>();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConsole();
+                    logging.SetMinimumLevel(LogLevel.Information);
+                });
+
+            if (useConsole)
+            {
+                await builder.RunConsoleAsync().ConfigureAwait(false);
+            }
+            else
+            {
+                await builder.RunAsServiceAsync().ConfigureAwait(false);
+            }
+        }
+    }
+}
